0a7957b51e07d89c0ea786af47e300beaf6441b7
Highbrook <Milivojevicdjole@yahoo.com>
2022-06-02 16:00:43 +0200

    simplified data fetching

--- script.js
+++ script.js
@@ -2,23 +2,24 @@ const listEl = document.getElementById("ul-el");
 const inputButtonEl = document.getElementById("input-btn");
 const exportButtonEl = document.getElementById("export-btn");
 const clearButtonEl = document.getElementById("clear-btn");
-const startSearchTerm = `<tbody class="MuiTableBody-root">`;
-const endSearchTerm = `<div class="jss148">`;
+//const startSearchTerm = `<tbody class="MuiTableBody-root">`;
+//const endSearchTerm = `<div class="jss148">`;
 //const trimStepOneStart = document.querySelector("#__next > main > div > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-4 > div.MuiGrid-root.MuiGrid-container.MuiGrid-item.MuiGrid-align-content-xs-flex-start.MuiGrid-grid-xs-12.MuiGrid-grid-md-8 > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-true > div > table > tbody");
 //const trimStepOneStart = `<div class="jss130">`;
 //const trimStepOneEnd = document.querySelector("#__next > main > div > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-4 > div.MuiGrid-root.MuiGrid-container.MuiGrid-item.MuiGrid-align-content-xs-flex-start.MuiGrid-grid-xs-12.MuiGrid-grid-md-8 > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-true > div > table > tbody");
 
-let scrapedData;
-let tempSnippet;
-let betweenStepsSnippet;
-let finalSnippet;
-let startIndexOfSearchTerm;
-let endIndexOfSearchTerm;
+// let scrapedData;
+// let tempSnippet;
+// let betweenStepsSnippet;
+// let finalSnippet;
+// let startIndexOfSearchTerm;
+// let endIndexOfSearchTerm;
 let scrapedArray = [];
 let addedItemNumber = 0;
 
 
-inputButtonEl.addEventListener("click", function () {
+inputButtonEl.addEventListener("click", async () => {
+	stringSlicer();
 });
 
 exportButtonEl.addEventListener("click", function () {
@@ -30,90 +31,29 @@ clearButtonEl.addEventListener("click", function () {
 });
 
 
-
-function stringSlicer(scrapedHTML) {
-
-	let startIndexOfStartSearchTerm = scrapedHTML.indexOf(startSearchTerm);
-	//let endIndexOfSearchTerm = scrapedHTML.lastIndexOf(endSearchTerm);
-	let endIndexOfEndSearchTerm = scrapedHTML.indexOf(endSearchTerm);
-	let firstStepSnippet = scrapedHTML.slice(startIndexOfStartSearchTerm, endIndexOfEndSearchTerm);
-	//console.log(betweenStepsSnippet);	//works
-	//let tempTableDataCheck = document.querySelector("#__next > main > div > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-4 > div.MuiGrid-root.MuiGrid-container.MuiGrid-item.MuiGrid-align-content-xs-flex-start.MuiGrid-grid-xs-12.MuiGrid-grid-md-8 > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-true > div > table > tbody");
-	testScrape();
-	
-
-
-	let tempTableDataCheck = document.querySelector('#__next > main > div ');
-	//console.log(tempTableDataCheck);
-
-	// let startIndexOfTrimStepOneStart = firstStepSnippet.indexOf(trimStepOneStart);
-	// console.log("startIndexOfTrimStepOneStart ", startIndexOfTrimStepOneStart);
-	// let endIndexOfTrimStepOneEnd = firstStepSnippet.lastIndexOf(trimStepOneEnd);
-	// console.log("endIndexOfTrimStepOneEnd ", endIndexOfTrimStepOneEnd);
-	// let secondStepSnippet = firstStepSnippet.slice(startIndexOfTrimStepOneStart, endIndexOfTrimStepOneEnd);
-
-	//console.log(secondStepSnippet);	//Doesnt work
-	//stringCleanup(secondStepSnippet);
-
+function stringSlicer() {
+	let tableData = [];
+	tableData = fetchTableData().then((value) => { console.log(value) });
 }
 
-async function testScrape(){
+async function fetchTableData() {
 	const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
 	const scraped = await chrome.scripting.executeScript(
 		{
 			target: { tabId: tabs[0].id },
-			func: function() {
-				 let tableFetch = document.querySelector("#__next > main > div > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-4 > div.MuiGrid-root.MuiGrid-container.MuiGrid-item.MuiGrid-align-content-xs-flex-start.MuiGrid-grid-xs-12.MuiGrid-grid-md-8 > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-true > div > table > tbody");
-				 let scrapedArrayData = [];
-				 //console.log(tableFetch.children[0].outerText);
+			func: function () {
+				let tableFetch = document.querySelector("#__next > main > div > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-4 > div.MuiGrid-root.MuiGrid-container.MuiGrid-item.MuiGrid-align-content-xs-flex-start.MuiGrid-grid-xs-12.MuiGrid-grid-md-8 > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-true > div > table > tbody");
+				let scrapedArrayData = [];
 				for (let i = 0; i < tableFetch.children.length; i++) {
 					const element = tableFetch.children[i].outerText;
 					scrapedArrayData.push(element);
 				}
 				return scrapedArrayData;
 			},
-			
 		})
-		console.log(scraped);
+	return scraped[0].result;
 }
 
-function stringCleanup(snippedString) {
-	let keyWordClosedBracket = `>`;
-	let keyWordOpenBracket = `<`;
-	let keyWordDatasheet = `Datasheet`;
-	let keyWordRef = `Customer Reference`;
-	let keyWordNBSP = `&nbsp;`;
-	let tempArray = [];
-	let tempFullArray = [];
-
-	for (let i = 0; i < snippedString.length; i++) {
-
-		if (snippedString.indexOf(keyWordClosedBracket, i) + 1 < snippedString.indexOf(keyWordOpenBracket, i)) {
-			let startOfFoundString = snippedString.indexOf(keyWordClosedBracket, i);
-			let endOfFoundString = snippedString.indexOf(keyWordOpenBracket, i);
-			let tempHolder = snippedString.slice(startOfFoundString + 1, endOfFoundString);
-			//console.log(tempHolder);
-			if (tempHolder != keyWordDatasheet && tempHolder != keyWordRef && tempHolder != keyWordNBSP) {
-				tempArray.push(tempHolder);
-
-				if (tempArray[tempArray.length - 1] != tempFullArray[tempFullArray.length - 1]) {
-					tempArray.pop();
-					tempFullArray.push(tempHolder);
-
-
-					if (tempFullArray.length > 20) {
-						endStringCleanup(tempFullArray);
-					} else {
-						tempArray.pop();
-						//console.log("Somethings fucky wucky");
-					}
-				}
-			}
-		}
-	}
-	endStringCleanup(tempFullArray);
-	add();
-}
 
 function add() {
 	scrapedArray.forEach(element => {
@@ -126,13 +66,13 @@ function add() {
 }
 
 function render() {
-	listEl.innerHTML = ''; 
+	listEl.innerHTML = '';
 	let items = Object.keys(localStorage);
 	//console.log(`lenght of items is: ` + items.length);
-	for(let key = 0; key < items.length; key++) {
-		if (localStorage.getItem(key.valueOf()) != "break-point"){
+	for (let key = 0; key < items.length; key++) {
+		if (localStorage.getItem(key.valueOf()) != "break-point") {
 			listEl.innerHTML += `<li>${localStorage.getItem(key)}</li>`;
-		}else if (localStorage.getItem(key.valueOf()) == "break-point"){
+		} else if (localStorage.getItem(key.valueOf()) == "break-point") {
 			listEl.innerHTML += `<br>`;
 		}
 	}
@@ -153,32 +93,78 @@ function clearList() {
 	addedItemNumber = 0;
 }
 
-function scrapeThePage() {
-	var htmlCode = document.documentElement.outerHTML;
-	return htmlCode;
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-	// Reaching the export-btn
-	const fbshare = document.querySelector('#input-btn')
-	fbshare.addEventListener('click', async () => {
-		// Get the active tab
-		const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
-		const scraped = await chrome.scripting.executeScript(
-			{
-				target: { tabId: tabs[0].id },
-				func: scrapeThePage,
-			})
-		// Result will be an array of values from the execution
-		// For testing this will be the same as the console output if you ran scriptToExec in the console
-		scrapedData = scraped[0].result;
-		stringSlicer(scrapedData);
-	})
-})
-
 window.addEventListener('DOMContentLoaded', () => {
-    render();
+	render();
 });
+
+
+
+
+
+
+
+
+
+// function stringCleanup(snippedString) {
+// 	let keyWordClosedBracket = `>`;
+// 	let keyWordOpenBracket = `<`;
+// 	let keyWordDatasheet = `Datasheet`;
+// 	let keyWordRef = `Customer Reference`;
+// 	let keyWordNBSP = `&nbsp;`;
+// 	let tempArray = [];
+// 	let tempFullArray = [];
+
+// 	for (let i = 0; i < snippedString.length; i++) {
+
+// 		if (snippedString.indexOf(keyWordClosedBracket, i) + 1 < snippedString.indexOf(keyWordOpenBracket, i)) {
+// 			let startOfFoundString = snippedString.indexOf(keyWordClosedBracket, i);
+// 			let endOfFoundString = snippedString.indexOf(keyWordOpenBracket, i);
+// 			let tempHolder = snippedString.slice(startOfFoundString + 1, endOfFoundString);
+// 			if (tempHolder != keyWordDatasheet && tempHolder != keyWordRef && tempHolder != keyWordNBSP) {
+// 				tempArray.push(tempHolder);
+
+// 				if (tempArray[tempArray.length - 1] != tempFullArray[tempFullArray.length - 1]) {
+// 					tempArray.pop();
+// 					tempFullArray.push(tempHolder);
+
+
+// 					if (tempFullArray.length > 20) {
+// 						endStringCleanup(tempFullArray);
+// 					} else {
+// 						tempArray.pop();
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// 	endStringCleanup(tempFullArray);
+// 	add();
+// }
+
+
+// function scrapeThePage() {
+// 	var htmlCode = document.documentElement.outerHTML;
+// 	return htmlCode;
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+// 	// Reaching the export-btn
+// 	const fbshare = document.querySelector('#input-btn')
+// 	fbshare.addEventListener('click', async () => {
+// 		// Get the active tab
+// 		const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
+// 		const scraped = await chrome.scripting.executeScript(
+// 			{
+// 				target: { tabId: tabs[0].id },
+// 				func: scrapeThePage,
+// 			})
+// 		// Result will be an array of values from the execution
+// 		// For testing this will be the same as the console output if you ran scriptToExec in the console
+// 		scrapedData = scraped[0].result;
+// 		stringSlicer(scrapedData);
+// 	})
+// })
+
 
 /////////////////// TESTING GROUND
 /*
