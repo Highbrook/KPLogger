b1254349b1a68269442a4f3ed94ccc4ee72518ae
Highbrook <MilivojevicDjole@yahoo.com>
2024-03-12 08:39:54 +0100

    reworked

--- script.js
+++ script.js
@@ -3,15 +3,6 @@ const inputButtonEl = document.getElementById("input-btn");
 const exportButtonEl = document.getElementById("export-btn");
 const clearButtonEl = document.getElementById("clear-btn");
 
-// Change this string to fit webpage element you wish to fetch
-const bodySelector = "#__next > div > div.ThreeColumnLayout_content__eTm7W > div > div > div.Grid_col-lg-10__FPLVk.Grid_col-xs__w58_v.Grid_col-sm__DsLxt.Grid_col-md__eg0dB > section.Box_box__CCDqC.AdPage_adInfoBox__oxywY"
-// Change this string to fit webpage title you wish to fetch
-const titleSelector = "#__next > div > div.ThreeColumnLayout_content__eTm7W > div > div > div.Grid_col-lg-10__FPLVk.Grid_col-xs__w58_v.Grid_col-sm__DsLxt.Grid_col-md__eg0dB > section.Box_box__CCDqC.AdPage_adInfoBox__oxywY > div.AdPage_adInfoHolder__wCKs4.AdPage_hasBorder__x5yTs > section.AdViewInfo_adViewInfoHolder__3OMT7 > h1"
-// Change this string to fit webpage price you wish to fetch
-const priceSelector = "#__next > div > div.ThreeColumnLayout_content__eTm7W > div > div > div.Grid_col-lg-10__FPLVk.Grid_col-xs__w58_v.Grid_col-sm__DsLxt.Grid_col-md__eg0dB > section.Box_box__CCDqC.AdPage_adInfoBox__oxywY > div.AdPage_adInfoHolder__wCKs4.AdPage_hasBorder__x5yTs > section.AdViewInfo_adViewInfoHolder__3OMT7 > div:nth-child(3) > h2"
-// Change this string to fit webpage body you wish to fetch
-const listingBodySelector = "#__next > div > div.ThreeColumnLayout_content__eTm7W > div > div > div.Grid_col-lg-10__FPLVk.Grid_col-xs__w58_v.Grid_col-sm__DsLxt.Grid_col-md__eg0dB > section.Box_box__CCDqC.AdPage_adInfoBox__oxywY > div.Grid_row__pl8x2 > div > section > div:nth-child(1) > p"
-
 inputButtonEl.addEventListener("click", async () => {
 	arrayShortener();
 });
@@ -27,7 +18,7 @@ clearButtonEl.addEventListener("click", function () {
 
 document.addEventListener('click', function (e) {
 	if (e.target && (e.target.id == 'delete-btn-label')) {
-		let targetChild = e.target.parentElement.parentElement.childNodes[1].childNodes[2].data;
+		let targetChild = e.target.parentElement.parentElement.childNodes[1].childNodes[1].childNodes[0].data
 		deleteIndividual(targetChild);
 	}
 });
@@ -35,69 +26,62 @@ document.addEventListener('click', function (e) {
 
 // fetch table data and create tableData object
 async function arrayShortener() {
-	let tableData = await fetchTableData().then((value) => {
-		let finalLenth = value.length - 2;
-		while (finalLenth < value.length) {
-			value.pop();
-		}
-		return value;
-	}
-	);
+	let tableData = await fetchTableData()
 	objectBuilder(tableData);
 }
 
 // fetching table data of listing from active chrome tab
 async function fetchTableData() {
 	const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
-	const scraped = await chrome.scripting.executeScript(
-		{
-			target: { tabId: tabs[0].id },
-			func: function () {
-				// tableFetch holds the JS Path to the table we want to fetch
-				let tableFetch = document.querySelector(bodySelector)
-				let scrapedArrayData = [];
-				for (let i = 0; i < tableFetch.children.length; i++) {
-					const element = tableFetch.children[i].outerText;
-					scrapedArrayData.push(element);
-				}
-				return scrapedArrayData;
-			},
-		})
+	const scraped = await chrome.scripting.executeScript({
+		target: { tabId: tabs[0].id },
+		func: function () {
+			// tableFetch holds the JS Path to the table we want to fetch
+			let scrapedArrayData = [];
+
+			// fetch title
+			let titleIndex = document.querySelector("#__next > div > div.ThreeColumnLayout_content__eTm7W > div > div > div.Grid_col-lg-10__FPLVk.Grid_col-xs__w58_v.Grid_col-sm__DsLxt.Grid_col-md__eg0dB > section.Box_box__CCDqC.AdPage_adInfoBox__oxywY > div.AdPage_adInfoHolder__wCKs4.AdPage_hasBorder__x5yTs > section.AdViewInfo_adViewInfoHolder__3OMT7 > h1")
+			let innerTitle = titleIndex.textContent || titleIndex.innerText || "";
+
+			// fetch price
+			let priceIndex = document.querySelector("#__next > div > div.ThreeColumnLayout_content__eTm7W > div > div > div.Grid_col-lg-10__FPLVk.Grid_col-xs__w58_v.Grid_col-sm__DsLxt.Grid_col-md__eg0dB > section.Box_box__CCDqC.AdPage_adInfoBox__oxywY > div.AdPage_adInfoHolder__wCKs4.AdPage_hasBorder__x5yTs > section.AdViewInfo_adViewInfoHolder__3OMT7 > div:nth-child(3) > h2")
+			let priceArray = priceIndex.innerHTML.split('<span>'[0]);
+			let innerPrice = priceArray[0];
+			//let innerPrice = price.textContent || price.innerText || "";
+
+			// fetch body
+			let temporatyBody = document.querySelector("#__next > div > div.ThreeColumnLayout_content__eTm7W > div > div > div.Grid_col-lg-10__FPLVk.Grid_col-xs__w58_v.Grid_col-sm__DsLxt.Grid_col-md__eg0dB > section.Box_box__CCDqC.AdPage_adInfoBox__oxywY > div.Grid_row__pl8x2 > div > section > div:nth-child(1) > p")
+			let innerBody = temporatyBody.textContent || temporatyBody.innerText || "";
+
+			scrapedArrayData.push(innerTitle);
+			scrapedArrayData.push(innerPrice);
+			scrapedArrayData.push(innerBody);
+
+			return scrapedArrayData;
+		},
+	})
 	return scraped[0].result;
 }
 
 // removing RegEx characters and creating an Object for local storage
 async function objectBuilder(arr) {
 	let completeListing = [];
-	arr.forEach(element => {
-		let listing = {
-			title: "",
-			price: "",
-			body: "",
-		};
+	let listing = {
+		title: "",
+		price: "",
+		body: "",
+	};
 
-		// fetch title
-		let titleIndex = document.querySelector(titleSelector)
-		let titleFinal = titleIndex.replaceAll(/\n\t|\n/g, " ");
-		listing.title = titleFinal;
+	listing.title = arr[0].replaceAll(/\n\t|\n/g, " ");
+	listing.price = arr[1].replaceAll(/\n\t|\n/g, " ");
+	listing.body = arr[2].replaceAll(/\n\t|\n/g, " ");
 
-		// fetch price
-		let priceIndex = document.querySelector(priceSelector)
-		let priceFinal = priceIndex.replaceAll(/\n\t|\n/g, " ");
-		listing.price = priceFinal;
+	completeListing.push(listing);
 
-		// fetch body
-		let temporatyBody = document.querySelector(listingBodySelector)
-		let bodyFinal = temporatyBody.replaceAll(/\n\t|\n/g, " ");
-		listing.body = bodyFinal;
-		completeListing.push(listing);
-	});
 
-	// fetching current tab URL
 	let dataToStore = convertToString(completeListing);
-	let listingName = completeListing[0].body.toString().replaceAll(/\s/g, '');
-	add(listingName, dataToStore);
-}
+	add(listing.title, dataToStore);
+};
 
 function convertToString(list) {
 	return JSON.stringify(list);
@@ -124,9 +108,6 @@ function render() {
 		for (let i = 0; i < parsedElement.length; i++) {
 			if ('title' in parsedElement[i]) {
 				addToListEl += `<b>${parsedElement[i].title}:</b> ${parsedElement[i].body}<br>`;
-				// TODO Check this ELSE IF, maybe I dont need it
-			} else if ('link' in parsedElement[i]) {
-				addToListEl += `<a id="link" href="${parsedElement[i].link}"><b>Link</b></a>`;
 			}
 		}
 		listEl.innerHTML += `
@@ -145,9 +126,12 @@ function render() {
 // removing item from local storage and re-rendering
 function deleteIndividual(itemToDelete) {
 	if (window.confirm("Are you sure you want to remove this listing?")) {
-		localStorage.removeItem(itemToDelete.toString().replaceAll(/\s/g, ''));
+		console.log(typeof(itemToDelete) )
+		let getItemKeyIndex = itemToDelete.split(':'[0]);
+		let getItemKey = getItemKeyIndex[0]
+		localStorage.removeItem(getItemKey.toString());
+		console.log(getItemKey)
 		render();
-		window.alert("listing removed!");
 	}
 }
 
@@ -173,21 +157,20 @@ function fetchFromStorage() {
 	let keys = Object.keys(localStorage);
 	let i = keys.length;
 	let text = '';
+	let foundTitle = ''
+	let foundPrice = ''
+	let foundBody = ''
 
 	keys.forEach(element => {
 		let returnedItem = localStorage.getItem(element);
 		let parsedItem = JSON.parse(returnedItem);
 
-		// these three values are required for my exported list, change these however you like
-		let expTitle = 'Title';
-		let expPrice = 'Price';
-		let expBody = 'Body';
-
-		let foundTitle = findInternalItem(parsedItem, expTitle);
-		let foundPrice = findInternalItem(parsedItem, expPrice);
-		let foundBody = findInternalItem(parsedItem, expBody);
-
-		text += `${parsedItem[0].body};${parsedItem[parsedItem.length - 1].link};${foundTitle};${foundPrice};${foundBody}\n`;
+		parsedItem.forEach(element => {
+			foundTitle = element.title;
+			foundPrice = element.price;
+			foundBody = element.body;
+		});
+		text += `${foundTitle};${foundPrice};${foundBody}\n`;
 	});
 
 	exportList(text)
